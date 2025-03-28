fb7c856b3538930c6453f5b71962aba12c2d4dd9
Lee Min Ju <69198709+minjuio@users.noreply.github.com>
2025-03-28 13:37:59 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.78 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,12 +1,21 @@
 function solution(s) {
-    let arr = s.split("");
-    let answer = [-1];
+//     let arr = s.split("");
+//     let answer = [-1];
 
-    for(let i=1; i<arr.length; i++) {
-        if(arr.slice(0, i).lastIndexOf(arr[i]) == -1) answer.push(-1);
-        else {
-            answer.push(i - arr.slice(0, i).lastIndexOf(arr[i]));  
-        }
-    }
+//     for(let i=1; i<arr.length; i++) {
+//         if(arr.slice(0, i).lastIndexOf(arr[i]) == -1) answer.push(-1);
+//         else {
+//             answer.push(i - arr.slice(0, i).lastIndexOf(arr[i]));  
+//         }
+//     }
+//     return answer;
+
+    let answer = [];
+    
+    [...s].map((char, i) => {
+        const count = s.slice(0, i).lastIndexOf(char);
+        count < 0 ? answer.push(-1) : answer.push(i - count);
+    });
+        
     return answer;
 }
